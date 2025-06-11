1272f6bcfa9c9b32d291e38757e65071a8b952a0
gnsdp99 <rladpgns99@gmail.com>
2025-06-12 00:42:09 +0900

    feat: API 요청

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,38 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState('')
+
+  const handleClick = async () => {
+    const response = await fetch('http://44.203.68.75:8080/api/hello')
+    const data = await response.json()
+    setData(data)
+  }
 
   return (
     <>
-      <h1>Hello World</h1>
-      <h3>Hello World</h3>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => handleClick()}>
+          Data from server: {data}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
   )
 }
